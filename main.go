f7f907a2637d5797852633088f398cf87dbdee06
HAYAMA_Kaoru <iyahaya@nifty.com>
2021-10-08 19:31:24 +0900

    On Ctrl-C terminate program without kill, and use filepath.Rel()

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"io/fs"
 	"os"
+	"os/signal"
 	"path/filepath"
 	"time"
 
@@ -14,41 +16,60 @@ const (
 	_STAMP_STYLE = "15:04:05.000"
 )
 
-func mains() error {
-	if dispose := colorable.EnableColorsStdout(nil); dispose != nil {
-		defer dispose()
-	}
-	out := colorable.NewColorableStdout()
-
-	tempPath := os.TempDir()
+func watch(rootPath string, out io.Writer) error {
 	previous := make(map[string]struct{})
-	filepath.Walk(tempPath, func(_path string, info fs.FileInfo, err error) error {
-		path := _path[len(tempPath):]
+	filepath.Walk(rootPath, func(path string, info fs.FileInfo, err error) error {
 		previous[path] = struct{}{}
 		return nil
 	})
 
-	tick := time.Tick(time.Second / 5)
-	for next := range tick {
-		current := make(map[string]struct{})
-		stamp := next.Format(_STAMP_STYLE)
+	ctrlc := make(chan os.Signal, 1)
+	signal.Notify(ctrlc, os.Interrupt)
+	defer signal.Reset(os.Interrupt)
 
-		filepath.Walk(tempPath, func(_path string, info fs.FileInfo, err error) error {
-			path := _path[len(tempPath):]
-			if _, ok := previous[path]; ok {
-				delete(previous, path)
-			} else {
-				fmt.Fprintf(out, "\x1B[32;1m%s Add %s\x1B[0m\n", stamp, path)
-			}
-			current[path] = struct{}{}
+	ticker := time.NewTicker(time.Second / 5)
+	defer ticker.Stop()
+
+	fmt.Fprintf(out, "\x1B[37;1mWatch Start: %s\x1B[0m\n", rootPath)
+	for {
+		select {
+		case <-ctrlc:
+			fmt.Fprintln(out, "\x1B[37;1mDone\x1B[0m")
 			return nil
-		})
-		for path := range previous {
-			fmt.Fprintf(out, "\x1B[31;1m%s Del %s\x1B[0m\n", stamp, path)
+		case next := <-ticker.C:
+			current := make(map[string]struct{})
+			stamp := next.Format(_STAMP_STYLE)
+
+			filepath.Walk(rootPath, func(path string, info fs.FileInfo, err error) error {
+				if _, ok := previous[path]; ok {
+					delete(previous, path)
+				} else {
+					relPath, err := filepath.Rel(rootPath, path)
+					if err != nil {
+						relPath = path
+					}
+					fmt.Fprintf(out, "\x1B[32;1m%s Add %s\x1B[0m\n", stamp, relPath)
+				}
+				current[path] = struct{}{}
+				return nil
+			})
+			for path := range previous {
+				relPath, err := filepath.Rel(rootPath, path)
+				if err != nil {
+					relPath = path
+				}
+				fmt.Fprintf(out, "\x1B[31;1m%s Del %s\x1B[0m\n", stamp, relPath)
+			}
+			previous = current
 		}
-		previous = current
 	}
-	return nil
+}
+
+func mains() error {
+	if dispose := colorable.EnableColorsStdout(nil); dispose != nil {
+		defer dispose()
+	}
+	return watch(os.TempDir(), colorable.NewColorableStdout())
 }
 
 func main() {
