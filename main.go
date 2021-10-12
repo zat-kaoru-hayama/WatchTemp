c41bd24855bc75ea9e2fac8d5330a0ed2645a4f5
HAYAMA_Kaoru <56809588+zat-kaoru-hayama@users.noreply.github.com>
2021-10-12 10:15:50 +0900

    Watch file modified

--- main.go
+++ main.go
@@ -16,10 +16,18 @@ const (
 	_STAMP_STYLE = "15:04:05.000"
 )
 
+type FileStatus struct {
+	ModTime time.Time
+	Size    int64
+}
+
 func watch(rootPath string, out io.Writer) error {
-	previous := make(map[string]struct{})
+	previous := make(map[string]FileStatus)
 	filepath.Walk(rootPath, func(path string, info fs.FileInfo, err error) error {
-		previous[path] = struct{}{}
+		previous[path] = FileStatus{
+			ModTime: info.ModTime(),
+			Size:    info.Size(),
+		}
 		return nil
 	})
 
@@ -37,20 +45,30 @@ func watch(rootPath string, out io.Writer) error {
 			fmt.Fprintln(out, "\x1B[37;1mDone\x1B[0m")
 			return nil
 		case next := <-ticker.C:
-			current := make(map[string]struct{})
+			current := make(map[string]FileStatus)
 			stamp := next.Format(_STAMP_STYLE)
 
 			filepath.Walk(rootPath, func(path string, info fs.FileInfo, err error) error {
-				if _, ok := previous[path]; ok {
+				relPath, err := filepath.Rel(rootPath, path)
+				if err != nil {
+					relPath = path
+				}
+				if pre, ok := previous[path]; ok {
+					if info != nil && !info.IsDir() && (pre.Size != info.Size() || pre.ModTime != info.ModTime()) {
+						fmt.Fprintf(out, "\x1B[33;1m%s Upd %s\x1B[0m\n", stamp, relPath)
+					}
 					delete(previous, path)
 				} else {
-					relPath, err := filepath.Rel(rootPath, path)
-					if err != nil {
-						relPath = path
-					}
 					fmt.Fprintf(out, "\x1B[32;1m%s Add %s\x1B[0m\n", stamp, relPath)
 				}
-				current[path] = struct{}{}
+				if info != nil {
+					current[path] = FileStatus{
+						ModTime: info.ModTime(),
+						Size:    info.Size(),
+					}
+				} else {
+					current[path] = FileStatus{}
+				}
 				return nil
 			})
 			for path := range previous {
